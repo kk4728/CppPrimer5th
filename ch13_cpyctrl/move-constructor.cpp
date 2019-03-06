2d10efffb6ad2babb9d7636841f790dfe4704ecd
kk4728 <rong.gong@foxmail.com>
2019-03-06 15:38:42 +0800

    chapter13 not finished.

--- ch13_cpyctrl/move-constructor.cpp
+++ ch13_cpyctrl/move-constructor.cpp
@@ -25,9 +25,46 @@ void f()
     A a4(a1, 100);
 }
 
+////////////////////////////////////////////
+struct X {
+	int i; //内置类型可移动
+	std::string s; //string定义了移动操作
+};
+
+struct hasX {
+	X mem; //X有合成的移动操作
+};
+
+void f1() {
+	X x;
+    //X &&x2 = x; //error 无法绑定左值
+    X x3 = std::move(x);
+	hasX hx, hx2 = std::move(hx);
+}
+
+////////////////////////////////////////////
+struct Y {
+    Y() = default;
+    Y(const Y&) = default;
+};
+struct hasY {
+    hasY() = default;
+    hasY(hasY &&) = default;
+    //hasY(const hasY &&) = default; //error
+    Y mem; 
+};
+
+void f2() {
+    hasY hy, hy2 = std::move(hy);
+}
+
+
+
 int main()
 {
-    f();
+    //f();
+    //f1();
+    f2();
 
     return 0;
 }
